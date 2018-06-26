a41175ded7746c97cf7c8d642669cd57acd9872c
Dan Oh <danoh82@gmail.com>
2018-06-26 13:58:55 -0700

    text animation, header text positioning, navbar css

--- src/Components/Carousel/Header_Carousel.jsx
+++ src/Components/Carousel/Header_Carousel.jsx
@@ -7,41 +7,43 @@ import dokdo5 from '../../Images/High_Res/hd_dokdo5.JPG';
 import dokdo6 from '../../Images/High_Res/hd_dokdo6.jpg';
 
 import dokdo10 from '../../Images/High_Res/hd_dokdo10.jpg';
+import Fade from 'react-reveal/Fade';
 import './Header_Carousel.css';
 
 class Header_Carousel extends Component {
     render() {
         return (
-            <Carousel style={{width: "100%"}}>
+            <Carousel interval={null} style={{
+                width: "100%"
+            }}>
                 <Carousel.Item>
-                    <img
-                        className='Header-Image'
-                        alt="900x500"
-                        src={dokdo}/>
-                    <Carousel.Caption>
-                        <h3>First slide label</h3>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+                    <img className='Header-Image' alt="900x500" src={dokdo}/>
+                    <Fade top>
+                        <Carousel.Caption className='Header-Content'>
+                            <h2 className='Header-Title'>Dokdo Island on a Clear day can be seen from Korea</h2>
+                            <p className='Header-Text'></p>
+                        </Carousel.Caption>
+                    </Fade>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img
-                        className='Header-Image'
-                        alt="900x500"
-                        src={dokdo1}/>
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+                    <img className='Header-Image' alt="900x500" src={dokdo1}/>
+                    <Fade top>
+                        <Carousel.Caption className='Header-Content'>
+                            <h2 className='Header-Title'>A part of Korea since AD 512</h2>
+                            <p className='Header-Text'></p>
+                        </Carousel.Caption>
+                    </Fade>
                 </Carousel.Item>
+
                 <Carousel.Item>
-                    <img
-                        className='Header-Image'
-                        alt="900x500"
-                        src={dokdo2}/>
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
+                    <img className='Header-Image' alt="900x500" src={dokdo2}/>
+                    <Fade top>
+                        <Carousel.Caption className='Header-Content'>
+                            <h2 className='Header-Title'>What's the deal with Dokdo?? Japan's fallacious story and malicious act with Dokdo... 
+                            </h2>
+                            <p className='Header-Text'></p>
+                        </Carousel.Caption>
+                    </Fade>
                 </Carousel.Item>
             </Carousel>
         )
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,8 +23,8 @@ class Dokdo_Navbar extends Component {
                         </NavItem>
                         <NavDropdown eventKey={3} title="Image Gallery" id="basic-nav-dropdown">
                             <MenuItem eventKey={3.1}>Action</MenuItem>
-                            <MenuItem eventKey={3.2}>Another action</MenuItem>
-                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem eventKey={3.2}></MenuItem>
+                            <MenuItem eventKey={3.3}></MenuItem>
                             <MenuItem divider/>
                             <MenuItem eventKey={3.3}>Separated link</MenuItem>
                         </NavDropdown>
