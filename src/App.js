375659696b974fc73cf56748bcf8ea99acad5138
Dan Oh <danoh82@gmail.com>
2018-06-26 12:06:54 -0700

    homepage n router setup started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Dokdo_Navbar from './Components/Navbar/Navbar';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
+import Home from './Views/Home/Home';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Dokdo_Navbar/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Dokdo_Navbar/>
+          <Home/>
+
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Views/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Home = (props) => {
+    return (
+        <div className='Homepage container' style={{marginTop: "100px"}}>
+           <p> HELLO FROM Home
+            1. Need Nice Hero section
+            2. short intro about website </p>
+        </div>
+    )
+}
+
+export default Home;
