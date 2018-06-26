e36fa82ec8a83e6e8a9366af079e89d508a7833b
Dan Oh <danoh82@gmail.com>
2018-06-26 11:27:44 -0700

    react app added with some essential packages, started navbar compo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Dokdo_Navbar from './Components/Navbar/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Dokdo_Navbar/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+
+class Dokdo_Navbar extends Component {
+    state = {}
+
+    render() {
+        return (
+            <Navbar inverse collapseOnSelect fixedTop>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#brand">Dokdo Island</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle/>
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1} href="#">
+                            About
+                        </NavItem>
+                        <NavItem eventKey={2} href="#">
+                            History
+                        </NavItem>
+                        <NavDropdown eventKey={3} title="Image Gallery" id="basic-nav-dropdown">
+                            <MenuItem eventKey={3.1}>Action</MenuItem>
+                            <MenuItem eventKey={3.2}>Another action</MenuItem>
+                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                            <MenuItem divider/>
+                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="#">
+                            Sign-in
+                        </NavItem>
+                        <NavItem eventKey={2} href="#">
+                            
+                        </NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+
+        )
+    }
+}
+
+
+export default Dokdo_Navbar;
