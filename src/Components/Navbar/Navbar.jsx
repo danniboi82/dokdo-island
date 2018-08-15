f810f81e998292b87781f7f799ac4639c278cc09
Dan Oh <danoh82@gmail.com>
2018-08-15 12:33:31 -0700

    adding additional pages and custom logo added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,52 +2,25 @@ import React, {Component} from 'react';
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import './Navbar.css';
+import logo from '../../Images/DokdoLogo1.png';
 class Dokdo_Navbar extends Component {
-    state = {
-        hover: false,
-    }
 
-
-    hoverHandler = (event) => {
-       this.setState({ hover : true})
-    }
     render() {
         return (
-            <Navbar inverse collapseOnSelect fixedTop className="Navbar" style={{}}>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <Link className="Navbar-Brand"  to="/" onMouseEnter={this.hoverHandler}>Dokdo Island</Link>
-                    </Navbar.Brand>
-                    <Navbar.Toggle/>
-                </Navbar.Header>
-                <Navbar.Collapse>
-                    <Nav>
-                        <NavItem eventKey={1} className="Navbar-Links">
-                            <Link to='/About'>About</Link>
-                        </NavItem>
-                        <NavItem eventKey={2} className="Navbar-Links">
-                            <Link to='/Image-Gallery'>Image Gallery</Link>
-                        </NavItem>
-                        <NavDropdown
-                            eventKey={3}
-                            title="Image Gallery"
-                            id="basic-nav-dropdown"
-                            className="Navbar-Links">
-                            <MenuItem eventKey={3.1} className="Navbar-Links">Action</MenuItem>
-                            <MenuItem eventKey={3.2} className="Navbar-Links"></MenuItem>
-                            <MenuItem eventKey={3.3} className="Navbar-Links"></MenuItem>
-                            <MenuItem divider/>
-                            <MenuItem eventKey={3.3} className="Navbar-Links">Separated link</MenuItem>
-                        </NavDropdown>
-                    </Nav>
-                    <Nav pullRight>
-                        <NavItem eventKey={1} href="#" className="Navbar-Links">
-                            Sign-in
-                        </NavItem>
-                        <NavItem eventKey={2} href="#" className="Navbar-Links"></NavItem>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <div className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <Link to='/' className="navbar-brand">
+                        <img src={logo} alt="" width='250px'/>
+                    </Link>
+                    <div className="navbar-nav nav-menu ml-auto">
+                        <Link to='/' className="nav-item">History</Link>
+                        <Link to='/signup' className="nav-item">Image Gallery</Link>
+                        <Link to='/signin' className="nav-item">Sign-in</Link>
+
+                    </div>
+
+                </div>
+            </div>
 
         )
     }
---
+++ src/Containers/Homepage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Homepage = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
